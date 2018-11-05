3cb24e286ae5615add17082deb1be661cf5ef502
Gabriel Dinca <33097664+GabrielDinca25@users.noreply.github.com>
2018-11-05 09:25:38 +0200

    Added: UI for in-game controls, menu functionality, player controller (for movement), added shuriken prefab

---
+++ Dungeon Game/Assets/Scripts/Common/Shuriken.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shuriken : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb2d;
+    private GameObject player;
+    private PlayerController playerControllerScript;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerControllerScript = player.GetComponent<PlayerController>();
+        rb2d = GetComponent<Rigidbody2D>();
+        if (playerControllerScript.facingRight)
+        {
+            rb2d.AddForce(transform.right * speed);
+            rb2d.angularVelocity = 200f;
+        }else
+        {
+            rb2d.AddForce(-transform.right * speed);
+            rb2d.angularVelocity = 200f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ground" || other.tag == "Breakable" || other.tag == "Climbable" || other.tag == "Environment")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
